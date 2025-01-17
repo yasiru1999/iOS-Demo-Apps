65e9062c1043ee861557741c1d27eb7ddc87015e
YASIRU JAYASEKARA <yasiru.j@aeturnum.com>
2025-01-17 21:44:54 +0530

    I am Poor App

---
+++ IAmPoor/I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by YASIRU JAYASEKARA on 2025-01-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IAmPoor/I Am Poor/I Am PoorTests/I_Am_PoorTests.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_PoorTests.swift
+//  I Am PoorTests
+//
+//  Created by YASIRU JAYASEKARA on 2025-01-17.
+//
+
+import Testing
+@testable import I_Am_Poor
+
+struct I_Am_PoorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

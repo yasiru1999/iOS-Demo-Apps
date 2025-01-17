20d5990ad7d725059702acf18a4c6b854311fa2e
YASIRU JAYASEKARA <yasiru.j@aeturnum.com>
2025-01-17 20:57:10 +0530

    I am Rich App

---
+++ IamRich/I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by YASIRU JAYASEKARA on 2025-01-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
